b699c12212ad2948a321d399330c4527d5913a71
roblkdeboer <roblkdeboer@gmail.com>
2023-04-02 23:11:06 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+    router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    log.Fatal(http.ListenAndServe("localhost:8081", router))
+
+}
