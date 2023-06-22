106b05e18e52d2ab0361cd865af55fd26ed7318f
roblkdeboer <roblkdeboer@gmail.com>
2023-06-22 15:27:48 +0800

    Tidying up project

--- cmd/login-module/main.go
+++ cmd/login-module/main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 
 	"github.com/roblkdeboer/login-module/internal/db"
 	"github.com/roblkdeboer/login-module/internal/handlers"
 	"github.com/roblkdeboer/login-module/internal/middleware"
-
-	_ "github.com/lib/pq"
 )
 
 func main() {
 
-	db, err := db.ConnectDB("localhost", 5455,"postgresUser", "postgresPW", "postgresDB")
+	err := godotenv.Load("../../.env")
+    if err != nil {
+        log.Fatal("error loading .env file", err)
+    }
+
+	portNum := os.Getenv("DB_PORT")
+	port, err := strconv.Atoi(portNum)
+	if err != nil {
+		log.Fatal("Error converting PORT to integer:", err)
+	}
+	
+	db, err := db.ConnectDB("localhost", port,os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
